f0ecfccacd01ebdd08db16c8bc56f4695a7ee2fc
Andrew Vy <andrew@andrewvy.com>
2015-11-05 14:28:23 -0800

    Add exrm deployment

--- mix.exs
+++ mix.exs
@@ -27,6 +27,8 @@ defmodule Chat.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:phoenix_ecto, "~> 1.1"},
      {:postgrex, ">= 0.0.0"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:exrm, "~> 0.19.9"}
+   ]
   end
 end
