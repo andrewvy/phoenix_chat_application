70d87e62a727503b2f524e59f4e4f12ce3a298df
Andrew Vy <andrew@andrewvy.com>
2015-11-05 15:34:49 -0800

    Try 0.0.2 hot code upgrade

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -30,7 +30,7 @@ defmodule Chat.RoomChannel do
     {:noreply, socket}
   end
   def handle_info(:ping, socket) do
-    push socket, "new:msg", %{user: "SYSTEM", body: "ping"}
+    push socket, "new:msg", %{user: "SYSTEM", body: "ping v2"}
     {:noreply, socket}
   end
 
